079052bf5b6c8fd5b8187555d3549392fec233e9
lsaa <lsaadev.personal@gmail.com>
2025-05-31 18:13:21 -0300

    Rocket Shoes fix + desc

--- ab_src/items/rocket_shoes.lua
+++ ab_src/items/rocket_shoes.lua
@@ -4,9 +4,18 @@
 ----------------------------------------------------------------------------
 local Item = include("ab_src.api.item")
 
-local rocketShoes = Item("Rocket Shoes", false)
+local desc = {
+    ["en_us"] = {"Rocket Shoes", "{{ArrowUp}} +0.1 Speed#{{SpeedSmall}} Instant acceleration to max speed"},
+    ["pt_br"] = {"Sapatos Foguete", "{{ArrowUp}} +0.1 Velocidade#{{SpeedSmall}} Aceleração isntantânea para a velocidade máxima"},
+}
 
-rocketShoes:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, function(player, player_type)
+local rocket_shoes = Item("Rocket Shoes", desc)
+
+rocket_shoes:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, function(player, player_type)
+    local count =  player:GetCollectibleNum(rocket_shoes.ID)
+    if count < 1 then
+        return
+    end
     if player:GetLastActionTriggers() & ActionTriggers.ACTIONTRIGGER_MOVED ~= 0 then
         local max_speed = player.MoveSpeed * 5
         if player.Velocity:Length() < max_speed then
@@ -17,8 +26,9 @@ rocketShoes:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, function(player, pl
     end
 end)
 
-rocketShoes:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, function(player, flag)
+rocket_shoes:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, function(player, flag)
+    local count =  player:GetCollectibleNum(rocket_shoes.ID)
     if flag == CacheFlag.CACHE_SPEED then
-        player.MoveSpeed = player.MoveSpeed + 0.1
+        player.MoveSpeed = player.MoveSpeed + 0.1 * count
     end
 end)

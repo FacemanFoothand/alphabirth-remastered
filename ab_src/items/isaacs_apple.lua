6b2b7aed0314f92d26c32ac0d766f73a12239839
lsaa <lsaadev.personal@gmail.com>
2025-06-01 17:53:37 -0300

    Add Isaac's apple description and buff it so it works on deli and hush

--- ab_src/items/isaacs_apple.lua
+++ ab_src/items/isaacs_apple.lua
@@ -3,14 +3,23 @@
 -- Originally from Pack 1
 -- Stops all tears in their tracks
 ----------------------------------------------------------------------------
-
 local Item = include("ab_src.api.item")
-local isaacs_apple = Item("Isaac's Apple")
+
+local desc = {
+    ["en_us"] = {"Isaac's Apple", "Makes all of the tears in a room drop on the spot"},
+    ["pt_br"] = {"Maçã do Isaac", "Faz todas as lágrimas em um quarto caírem no lugar"},
+}
+
+local isaacs_apple = Item("Isaac's Apple", desc)
 
 isaacs_apple:AddCallback(ModCallbacks.MC_USE_ITEM, function(id, rng, player)
 	for _, entity in ipairs(Isaac:GetRoomEntities()) do
-		if entity.Type == EntityType.ENTITY_TEAR or entity.Type == EntityType.ENTITY_PROJECTILE then
+		if entity.Type == EntityType.ENTITY_TEAR then
 			entity.Velocity = Vector.Zero
+		end
+		if entity.Type == EntityType.ENTITY_PROJECTILE then
+			entity.Velocity = Vector.Zero
+            entity:ToProjectile():AddFallingAccel(6.0)
 		end
 	end
 end)

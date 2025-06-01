4958861295efc15fdc5fa45cebe201647c983081
lsaa <lsaadev.personal@gmail.com>
2025-06-01 01:31:05 -0300

    Fix entropy + desc

--- ab_src/items/entropy.lua
+++ ab_src/items/entropy.lua
@@ -5,39 +5,38 @@
 local Item = include("ab_src.api.item")
 local utils = include("ab_src.modules.utils")
 
-local entropy = Item("Entropy", false)
+local desc = {
+    ["en_us"] = {"Entropy", "{{ArrowUp}} 1.5x Tears multiplier#Chance to fire an extra tear with a slightly off trajectory#{{LuckSmall}} 100% Chance at 7 Luck"},
+    ["pt_br"] = {"Entropia", "{{ArrowUp}} 1.5x Multiplicador de Lágrimas#Chance de atirar uma lágrima extra com trajetória diferente%{{LuckSmall}} 100% de chance com 7 Sorte"},
+}
+
+local entropy = Item("Entropy", desc)
+
+local entropy_flag = false
 
 entropy:AddCallback(ModCallbacks.MC_POST_TEAR_INIT, function(entity)
     local data = entity:GetData()
     local player = entity:GetLastParent():ToPlayer()
-    if not data.isEntropyTear and utils.getLuckRNG(player, 66, 5) then
-        local angle = entity.Velocity:GetAngleDegrees()
-        local length = entity.Velocity:Length()
-        -- local oldTear = entity:ToTear()
-        -- local flags = oldTear.TearFlags
-        local variance = 10 
-        -- local entropyTears = {oldTear}
-        local tear = player:FireTear(player.Position, Vector.FromAngle(angle + utils.random(-variance,variance)):Resized(length), true, false, false)
-        tear:GetData().isEntropyTear = true
+    if not entropy_flag and utils.getLuckRNG(player, 66, 5) then
+        local angle = player:GetAimDirection():GetAngleDegrees()
+        local avoid_center = 6
+        local variance = 15
+        local deviation
+        if utils.random(0, 1) == 0 then
+            deviation = utils.random(-variance, -avoid_center)
+        else
+            deviation = utils.random(avoid_center, variance)
+        end
+        local length = player.ShotSpeed * 10.0 + entity.Velocity:Length()
 
-        -- for _, flag in ipairs(splitFlags) do
-        --     if flags & flag == flag then
-        --         if AlphaAPI.getLuckRNG(66, 5) then
-        --             local tear = player:FireTear(player.Position, Vector.FromAngle(angle + random(-variance,variance)):Resized(length), true, false, false)
-        --             AlphaAPI.addFlag(tear, ENTITY_FLAGS.ENTROPY_TEAR)
-        --             entropyTears[#entropyTears + 1] = tear
-        --         end
-        --     end
-        -- end
-
-        -- local tear = player:FireTear(player.Position, Vector.FromAngle(angle + random(-variance,variance)):Resized(length), true, false, false)
-        -- entropyTears[#entropyTears + 1] = tear
-        -- AlphaAPI.addFlag(tear, ENTITY_FLAGS.ENTROPY_TEAR)
-
-        -- for _, tear in ipairs(entropyTears) do
-        --     tear.TearFlags = tear.TearFlags & ~allSplitFlag
-        -- end
+        entropy_flag = true
+        player:FireTear(
+            player.Position,
+            Vector.FromAngle(angle + deviation):Resized(length),
+            true, false, false
+        )
     end
+    entropy_flag = false
 end)
 
 entropy:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, function(player, flag)

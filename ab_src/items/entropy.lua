776da2b1e77afd092723161036aeaf41117a81c3
lsaa <lsaadev.personal@gmail.com>
2025-06-01 17:10:47 -0300

    Get entropy to work with lasers

--- ab_src/items/entropy.lua
+++ ab_src/items/entropy.lua
@@ -2,6 +2,7 @@
 -- Item: Entropy
 -- Originally from Pack 3
 ----------------------------------------------------------------------------
+local g = require("ab_src.modules.globals")
 local Item = include("ab_src.api.item")
 local utils = include("ab_src.modules.utils")
 
@@ -36,6 +37,41 @@ entropy:AddCallback(ModCallbacks.MC_POST_TEAR_INIT, function(entity)
             true, false, false
         )
     end
+    entropy_flag = false
+end)
+
+entropy:AddCallback(ModCallbacks.MC_POST_LASER_INIT, function(laser)
+    local player = laser:GetLastParent():ToPlayer()
+    if not player then return end
+
+    if not entropy_flag and utils.getLuckRNG(player, 66, 5) then
+        local angle = player:GetLastDirection():GetAngleDegrees()
+        local avoid_center = 5
+        local variance = 7
+        local deviation
+
+        if utils.random(0, 1) == 0 then
+            deviation = utils.random(-variance, -avoid_center)
+        else
+            deviation = utils.random(avoid_center, variance)
+        end
+
+        local new_angle = angle + deviation
+        local velocity_vector = Vector.FromAngle(new_angle)
+        local length = player.ShotSpeed * 10.0 + player.Velocity:Length()
+        local adjusted_velocity = velocity_vector:Resized(length)
+
+        entropy_flag = true
+
+        if player:HasWeaponType(WeaponType.WEAPON_BRIMSTONE) then
+            player:FireBrimstone(velocity_vector, player, 1.0)
+        elseif player:HasWeaponType(WeaponType.WEAPON_LASER) then
+            player:FireTechLaser(player.Position, LaserOffset.LASER_TECH2_OFFSET, velocity_vector, false, false, player, 1.0)
+        elseif player:HasWeaponType(WeaponType.WEAPON_TECH_X) then
+            player:FireTechXLaser(player.Position, adjusted_velocity, 40.0, player, 1.0)
+        end
+    end
+
     entropy_flag = false
 end)
 
